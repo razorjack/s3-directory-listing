8f840ba441c66488214d2ca135255d5ad999fafa
Jacek Galanciak <jacek.galanciak@gmail.com>
2017-07-30 10:26:19 +0200

    Webpack, babel, es6

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const aws = require('aws-sdk');
-const path = require('path');
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+// var S3 = require('aws-sdk/clients/s3');
+import S3 from 'aws-sdk/clients/s3';
+import path from 'path';
+const s3 = new S3({ apiVersion: '2006-03-01' });
 
-module.exports.generateListing = (event, context, callback) => {
+export const generateListing = function(event, context, callback) {
+  console.log("New version!");
   // Get the object from the event and show its content type
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './handler.js',
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }]
+  }
+};
